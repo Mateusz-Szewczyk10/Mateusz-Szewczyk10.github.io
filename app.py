4c23b203da4eb14ef8e1d6c532b45a97d106a08f
Mateusz <mateuszs1803@gmail.com>
2023-04-02 16:13:42 +0200

    Fixed ML implementation and nav bar

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ model = pickle.load(open("model.pkl", "rb"))
 
 @flask_app.route("/")
 def Home():
-    return render_template("index.html")
+    return render_template("ML_model_page.html")
 
 @flask_app.route("/predict", methods = ["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+    return render_template("ML_model_page.html", prediction_text = "The flower species is {}".format(prediction))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
